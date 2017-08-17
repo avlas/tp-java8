ee79f8be2afe7db51ca8b9755c634192a210a890
Avl <alina.vlas@gmail.com>
2017-08-17 18:56:05 +0200

    Anonymous implementation

--- src/test/java/java8/ex03/Lambda_03_Test.java
+++ src/test/java/java8/ex03/Lambda_03_Test.java
@@ -1,45 +1,66 @@
 package java8.ex03;
 
-import java8.data.Data;
-import java8.data.Person;
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+
+import org.junit.Test;
+
+import java8.data.Data;
+import java8.data.Person;
 
 /**
  * Exercice 03 - ForEach
  */
 public class Lambda_03_Test {
 
-    // tag::PersonProcessor[]
-    interface PersonProcessor {
-        void process(Person p);
-    }
-    // end::PersonProcessor[]
+	// tag::PersonProcessor[]
+	interface PersonProcessor {
+		void process(Person p);
+	}
+	// end::PersonProcessor[]
 
-    // tag::forEach[]
-    private void forEach(List<Person> source, PersonProcessor processor) {
-       // TOD0
-    }
-    // end::forEach[]
+	// tag::forEach[]
+	private void forEach(List<Person> source, PersonProcessor processor) {
+		// TOD0
+		for (Person person : source) {
+			processor.process(person);
+		}
+	}
+	// end::forEach[]
 
+	// tag::test_verify_person[]
+	@Test
+	public void test_verify_person() throws Exception {
 
-    // tag::test_verify_person[]
-    @Test
-    public void test_verify_person() throws Exception {
+		List<Person> personList = Data.buildPersonList(100);
 
-        List<Person> personList = Data.buildPersonList(100);
+		// TODO vérifier qu'une personne à un prénom qui commence par first
+		// TODO vérifier qu'une personne à un nom qui commence par last
+		// TODO vérifier qu'une personne à un age > 0
+		// TODO la vérification se fait via une assertion (mot clé assert)
+		PersonProcessor verifyPerson = new PersonProcessor() {
+			public void process(Person person) {
+				assertTrue(person.getFirstname().startsWith("first"));
+			}
+		};
 
-        // TODO vérifier qu'une personne à un prénom qui commence par first
-        // TODO vérifier qu'une personne à un nom qui commence par last
-        // TODO vérifier qu'une personne à un age > 0
-        // TODO la vérification se fait via une assertion (mot clé assert)
-        PersonProcessor verifyPerson = null;
+		verifyPerson = new PersonProcessor() {
+			public void process(Person person) {
+				assertTrue(person.getLastname().startsWith("last"));
+			}
+		};
 
-        assert verifyPerson != null;
+		verifyPerson = new PersonProcessor() {
+			public void process(Person person) {
+				assertTrue(person.getAge() > 0);
+			}
+		};
 
-        forEach(personList, verifyPerson);
-    }
-    // end::test_verify_person[]
+		assert verifyPerson != null;
+
+		forEach(personList, verifyPerson);
+	}
+	// end::test_verify_person[]
 
 }
