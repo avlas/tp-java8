cca85770b274e8f226fb2f289d76f9e891ac101d
Avl <alina.vlas@gmail.com>
2017-08-18 10:29:29 +0200

    Stream implementations

--- src/test/java/java8/tp/BasketTest.java
+++ src/test/java/java8/tp/BasketTest.java
@@ -1,6 +1,9 @@
 package java8.tp;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -25,6 +28,31 @@ public class BasketTest {
 		basket.getArticles().add(new Article("Camembert", "Produits laitiers", 2.8, false));
 		basket.getArticles().add(new Article("Yaourth", "Produits laitiers", 3.1, false));
 
+		Stream<Article> stream = basket.getArticles().stream();
+		
+		double result = stream.filter(a-> a.getPrice() > 3.0)
+				  .map(a -> a.getPrice())
+				  .reduce((a,b)-> a+b)
+				  .get();	// optional = conteneur du resultat
+		
+		// Equivalent way
+		// intermediate operations => no treatement
+		stream = basket.getArticles().stream();
+		Stream<Article> articleStream = stream.filter(a-> a.getPrice() > 3.0);		
+		
+		Stream<Double> priceStream = articleStream.map(a -> a.getPrice());
+		
+		// final operation => treatement
+		result = priceStream.reduce((a,b)-> a+b).get();	// optional = conteneur du resultat
+
+		stream = basket.getArticles().stream();
+		List<Article> articlesFilters = stream.filter(a-> a.getPrice() > 3.0)
+															.collect(Collectors.toList());	
+		
+		stream = basket.getArticles().stream(); 	// error Execution si on ne reinitialise pas le stream, car l'ancien a ete deja ferme 
+		List<Article> articlesFilters2 = stream.filter(a-> a.getPrice() < 3.0)
+											 	.collect(Collectors.toList());	
+		
 		return basket;
 	}
 
@@ -32,7 +60,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_inStock() throws Exception {
 		System.out.println("Impérative way - Articles in stock : ");
-		
+
 		filteredList = getBasket().filterArticlesByStock();
 		assert filteredList.size() == 6;
 	}
@@ -57,7 +85,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_filterInStock() throws Exception {
 		System.out.println("\nFilter way - Articles in stock : ");
-		
+
 		filteredList = getBasket().filter(new FilterByStockImpl());
 		assert filteredList.size() == 6;
 	}
@@ -65,7 +93,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_filterInStockAndPrice() throws Exception {
 		System.out.println("\nFilter way - Articles in stock, less then maxPrice : ");
-		
+
 		filteredList = getBasket().filter(new FilterByStockAndPriceImpl());
 		assert filteredList.size() == 1;
 	}
@@ -74,7 +102,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_anonymeFilterInStock() throws Exception {
 		System.out.println("\nAnonymous way - Articles in stock : ");
-		
+
 		filteredList = getBasket().filter(new Filter() {
 			@Override
 			public boolean accept(Article article) {
@@ -87,7 +115,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_anonymeFilterInStockAndPrice() throws Exception {
 		System.out.println("\nAnonymous way - Articles in stock, less then maxPrice : ");
-		
+
 		filteredList = getBasket().filter(new Filter() {
 			@Override
 			public boolean accept(Article article) {
@@ -101,7 +129,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_lambdaFilterInStock() throws Exception {
 		System.out.println("\nLambda way - Articles in stock : ");
-		
+
 		filteredList = getBasket().filter(article -> !article.isOutOfStock());
 		assert filteredList.size() == 6;
 	}
@@ -109,7 +137,7 @@ public class BasketTest {
 	@Test
 	public void test_filter_lambdaFilterInStockAndPrice() throws Exception {
 		System.out.println("\nLambda way - Articles in stock, less then maxPrice : ");
-		
+
 		filteredList = getBasket().filter(article -> (!article.isOutOfStock() && (article.getPrice() < 2.0)));
 		assert filteredList.size() == 1;
 	}
