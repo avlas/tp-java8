6eb92aad2669077624a1f55e167c8e17c1585bed
Avl <alina.vlas@gmail.com>
2017-08-18 17:40:57 +0200

    implement Test1

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -17,14 +17,13 @@ public class DateAndTime_01_Test {
     @Test
     public void test_date() {
 
-        // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
-
+        // Modifier les paramètres pour que le test soit passant
+        Date date = new Date(117, 2, 24, 1, 2, 3);
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
-        // TODO modifier le jour de la date pour que le test soit passant
-
+        // Modifier le jour de la date pour que le test soit passant
+        date.setDate(25);
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
@@ -34,13 +33,19 @@ public class DateAndTime_01_Test {
     public void test_calendar() throws Exception {
         Calendar calendar = Calendar.getInstance();
 
-        // TODO modifier l'objet calendar pour que le test soit passant
-
+        // Modifier l'objet calendar pour que le test soit passant
+        calendar.set(Calendar.YEAR, 2017);
+        calendar.set(Calendar.MONTH, 2);
+        calendar.set(Calendar.DAY_OF_MONTH, 24);
+        calendar.set(Calendar.HOUR_OF_DAY, 1);
+        calendar.set(Calendar.MINUTE, 2);
+        calendar.set(Calendar.SECOND, 3);        
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
-        // TODO modifier l'objet calendar pour que le test soit passant
-
+        // Modifier l'objet calendar pour que le test soit passant
+        // calendar.set(Calendar.DAY_OF_MONTH, 25);
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
